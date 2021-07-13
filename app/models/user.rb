11a75e3a4d44d4a0bab9e5df1fb8a80b80f0d0c4
DanteAlonsoHT <dante1818@hotmail.com>
2021-07-13 09:26:59 -0500

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,17 +13,17 @@ class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
 
-  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
   has_many :pending_friends, through: :pending_friendships, source: :friend
 
-  has_many :inverted_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships
 
   def friends_and_own_posts
-    Post.where(user: (self.friends.to_a << self))
+    Post.where(user: (friends.to_a << self))
   end
 
   def friends

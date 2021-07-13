e5a53e90c20efa79c87b4e52ce9300070a2e2880
DanteAlonsoHT <dante1818@hotmail.com>
2021-07-13 09:18:11 -0500

    Using more SQL Queries instead ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
-
-  has_many :posts
   has_many :posts, dependent: :destroy
+
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
   has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
   has_many :friends, through: :confirmed_friendships
+
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :friend_requests, through: :inverted_friendships
+
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
